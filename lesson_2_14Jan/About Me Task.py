dbfb23011fec4ebf777c71ac8048474415c5e79b
Ocean_Graham <oceanvgrahambusiness@gmail.com>
2025-01-16 12:01:15 +0000

    removal of lesson files, all input into 403 directory

--- lesson_2_14Jan/About Me Task.py
+++
@@ -1,5 +0,0 @@
-print ("My name is Ocean. \n\nMy Surname is Graham")
-
-
-print ("This is another way to make a space.",  end="\n\n")
-print ("here is the next line")
--- lesson_4_16Jan/multiplication table alternative.py
+++
@@ -1,8 +0,0 @@
-for i in range (1, 11):
-    j = 1
-    print ("\nThis is the", {i}, "times table up to 10")
-    while j <= 10:
-        print (f"{j*i}", end= "")
-        j += 1
-        print()
-   
