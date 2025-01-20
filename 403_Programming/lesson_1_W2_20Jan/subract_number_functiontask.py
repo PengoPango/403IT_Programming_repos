917cc34a89435c046fa53cb942f9319e35049253
Ocean_Graham <oceanvgrahambusiness@gmail.com>
2025-01-20 12:26:45 +0000

    Lesson 1 Week 2 added

---
+++ 403_Programming/lesson_1_W2_20Jan/subract_number_functiontask.py
@@ -0,0 +1,10 @@
+def subract_numbers (a, b):
+
+    return a - b
+
+if __name__ == "__main__":
+    while True:
+        try:
+            input_a = input ("Enter the first number")
+            input_b = input ("Enter the second number")
+             
