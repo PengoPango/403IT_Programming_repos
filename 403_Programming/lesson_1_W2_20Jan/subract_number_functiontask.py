840402492d78596f6091412f7964faa119923058
Ocean_Graham <oceanvgrahambusiness@gmail.com>
2025-01-21 09:22:33 +0000

    Added week2 lesson 1 subraction task

--- 403_Programming/lesson_1_W2_20Jan/subract_number_functiontask.py
+++ 403_Programming/lesson_1_W2_20Jan/subract_number_functiontask.py
@@ -5,6 +5,20 @@ def subract_numbers (a, b):
 if __name__ == "__main__":
     while True:
         try:
-            input_a = input ("Enter the first number")
-            input_b = input ("Enter the second number")
-             
+            input_a = (int(input("Enter the first number:")))
+            input_b = (int(input("Enter the second number:")))
+            a = (int(input_a))
+            b = (int(input_b))
+            result = (a - b)
+            print (f"the result of {a} subract {b} is {result}")
+            break
+
+        except ValueError:
+            print("Please enter a valid number")
+            
+
+            
+
+        
+
+    
