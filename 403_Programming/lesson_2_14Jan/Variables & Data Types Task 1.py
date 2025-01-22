8fab6590a5d76aed26fcdd6319c032e7dd9972df
Ocean_Graham <oceanvgrahambusiness@gmail.com>
2025-01-22 11:30:25 +0000

    rm test

--- 403_Programming/lesson_2_14Jan/Variables & Data Types Task 1.py
+++
@@ -1,7 +0,0 @@
-# write a program that outputs a personalized introduction using variables.
-
-name = str(input("What is your name"))
-
-age = int(input("How old are you?"))
-
-print ("My name is", name, "\n I am", age, "years old")
